3340885384183b87abf9e840eaea3654b5d255ce
Presty12 <73420849+Presty12@users.noreply.github.com>
2021-01-12 11:44:32 +0300

    Update sunucuAyar.js

--- sunucuAyar.js
+++ sunucuAyar.js
@@ -1,13 +1,13 @@
 const ayarlar = {
-  guildID: '790861621386936330', // sunucu idsi
-  ownerRole:"790861621743714355", // sunucu sahiplerinin rolü
-  teyitsizRolleri:['790861621479211017'], // kayıt olmayanlara verieln rol
-  jailRolu:"790861621676605445", // cezalılara verilen rol
-  enAltYetkiliRolu:['790861621648031768'], // ilk yetkili permi
-  activity:"S'anta", // botun durumu
-  status:"online",
-  invitelink:"https://discord.gg/NMw3k9QbFs", //davet linki
-  symbol:"✩", // tag
-  voicechannel:"795377106426658836" // botun gireceği ses kanalı
+  guildID: 'id', // sunucu idsi
+  ownerRole:"id", // sunucu sahiplerinin rolü
+  teyitsizRolleri:['id'], // kayıt olmayanlara verieln rol
+  jailRolu:"id", // cezalılara verilen rol
+  enAltYetkiliRolu:['id'], // ilk yetkili permi
+  activity:"shina", // botun durumu
+  status:"online",// dnd idle / dnd = rahatsız etmeyin idle = boşta
+  invitelink:"davet", //davet linki
+  symbol:"tag", // tag
+  voicechannel:"id" // botun gireceği ses kanalı
 };
 module.exports = ayarlar;
